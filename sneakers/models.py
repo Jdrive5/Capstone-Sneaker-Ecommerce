8e3037868b0d2fff51388327ba246654e7415103
Monica Banda <monicabanda@Monicas-Air.lan>
2023-02-20 14:22:48 -0700

    Created Sneaker Serializer Class and File

--- sneakers/models.py
+++ sneakers/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Sneaker(models.Model):
     name = models.CharField(max_length=255)
     style = models.CharField(max_length=255)
-    brand = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    
