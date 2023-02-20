1fdee20375c45a44952bb3fd807adaf533864cb5
Monica Banda <monicabanda@Monicas-Air.lan>
2023-02-20 13:39:30 -0700

    Created Sneaker Model for Sneakers App + Registered Sneakers and Rest Framework in Project Settings Installed Apps Sections

--- sneakers/models.py
+++ sneakers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Sneaker(models.Model):
+    name = models.CharField(max_length=255)
+    style = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
